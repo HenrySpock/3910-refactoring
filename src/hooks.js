0461739948c06e89434b38235e860d694d9008e6
HenrySpock <motiviquestudio@gmail.com>
2023-08-22 22:48:10 -0500

    Part 02 useAxios for PlayingCardList

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function useFlip(initialFlipState = true) {
     const [isFlipped, setIsFlipped] = useState(initialFlipState);
@@ -9,4 +10,17 @@ function useFlip(initialFlipState = true) {
     return [isFlipped, toggleFlip];
 }
 
-export { useFlip };
+function useAxios(url) {
+    const [data, setData] = useState([]);
+
+    const addData = async () => {
+        const response = await axios.get(url);
+        setData(oldData => [...oldData, response.data]);
+    };
+
+    return [data, addData];
+}
+
+export { useFlip, useAxios };  
+
+ 
